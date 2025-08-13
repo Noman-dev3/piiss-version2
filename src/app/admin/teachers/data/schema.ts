36a691cbfda598b02cc7e4590e27275983272ba0
Python Developer <noman.dev3@gmail.com>
2025-08-13 20:32:45 +0000

    hey please the web dont load data like annoucemnts ,events,teachers and

--- src/app/admin/teachers/data/schema.ts
+++ src/app/admin/teachers/data/schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const teacherSchema = z.object({
   id: z.string(),
   name: z.string(),
-  bio: z.string().optional(),
+  bio: z.string().optional().nullable(),
   contact: z.string(),
   dateJoined: z.string(),
   department: z.string(),
