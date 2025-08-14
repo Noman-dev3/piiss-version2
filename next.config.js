8b610acf1e6c353c98ada520dd12a9680fdefb0a
Python Developer <noman.dev3@gmail.com>
2025-08-14 18:16:19 +0000

    great now why this happen(General HTTP error 1 instance We encountered a

--- next.config.js
+++ next.config.js
@@ -9,6 +9,24 @@ const nextConfig = {
       },
     ],
   },
+  sitemap: async () => {
+    const baseUrl = 'https://piiss.vercel.app';
+    const staticRoutes = [
+      '/',
+      '/admissions',
+      '/results',
+      '/events',
+      '/faculty',
+      '/gallery',
+    ];
+
+    return staticRoutes.map((route) => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: route === '/' ? 1 : 0.8,
+    }));
+  },
 };
 
 module.exports = nextConfig;
