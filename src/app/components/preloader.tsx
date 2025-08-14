e3fc9f488960713b8ceb47812ea5849e17e9f9a7
Python Developer <noman.dev3@gmail.com>
2025-08-14 07:39:08 +0000

    remove 3d loder but Create a smooth, looping "finger wave" loader animat

--- src/app/components/preloader.tsx
+++ src/app/components/preloader.tsx
@@ -1,22 +1,31 @@
 
 "use client";
 
-import { Canvas } from "@react-three/fiber";
-import { PuzzleCube } from "./puzzle-cube";
+import React from 'react';
+
+const FingerWaveLoader = () => {
+  return (
+    <div className="loader-container">
+      <div className="hand">
+        <div className="finger finger-1"></div>
+        <div className="finger finger-2"></div>
+        <div className="finger finger-3"></div>
+        <div className="finger finger-4"></div>
+        <div className="palm"></div>
+        <div className="thumb"></div>
+      </div>
+    </div>
+  );
+};
+
 
 export const Preloader = () => {
   return (
     <div className="preloader">
-      <div className="h-48 w-48">
-        <Canvas>
-          <ambientLight intensity={1.5} />
-          <pointLight position={[10, 10, 10]} />
-          <PuzzleCube />
-        </Canvas>
-      </div>
-      <p className="text-lg font-medium text-muted-foreground animate-pulse">
-        Loading PIISS...
-      </p>
+        <div className="wave-loader-wrapper">
+             <FingerWaveLoader />
+            <div className="loader-text">LOADING</div>
+        </div>
     </div>
   );
 };
