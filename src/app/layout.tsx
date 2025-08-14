0a01f4f47ccddae46cef0d5595fd6c4116fdb666
Python Developer <noman.dev3@gmail.com>
2025-08-14 18:04:07 +0000

    navigation from 1 to another page is more slower than a turtle on surfac

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,6 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Preloader } from "@/app/components/preloader";
-import { useState, useEffect } from "react";
 import Script from "next/script";
 
 const ptSans = PT_Sans({ 
@@ -22,23 +20,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // Simulate loading time
-
-    return () => clearTimeout(timer);
-  }, []);
-
-
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
         <title>PIISS - Pakistan Islamic International School System | Buner, Swari</title>
         <meta name="description" content="Pakistan Islamic International School System (PIISS) in Swari, Buner, offers excellence in education with a blend of academic rigor and Islamic values. Enquire about admissions today." />
-        <meta name="keywords" content="PIISS, Pakistan Islamic International School System, school in Buner, school in Swari, education in Buner, PIISS admissions, Islamic school, quality education, private school Buner, Buner Swari school, admission open" />
+        <meta name="keywords" content="PIISS, Pakistan Islamic International School System, school in Buner, school in Swari, education in Buner, PIISS admissions, Islamic school, quality education, private school Buner, Buner Swari school, admission open, school data, student results, faculty information, school events, gallery" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
          <Script
           async
@@ -66,10 +53,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {loading && <Preloader />}
-          <div className={cn(loading ? 'hidden' : 'block')}>
-            {children}
-          </div>
+          {children}
           <Toaster />
         </ThemeProvider>
       </body>
