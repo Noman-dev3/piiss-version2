ac576a6510bebc37e1396f02fac431c58df728b6
Python Developer <noman.dev3@gmail.com>
2025-08-14 08:06:49 +0000

    remove the loader and instead create a loader html( <div class="loader">

--- src/app/components/preloader.tsx
+++ src/app/components/preloader.tsx
@@ -4,11 +4,7 @@
 export const Preloader = () => {
   return (
     <div className="preloader">
-      <div className="hourglass-loader">
-        <div className="hourglass__top"></div>
-        <div className="hourglass__bottom"></div>
-        <div className="hourglass__line"></div>
-      </div>
+      <div className="loader"></div>
       <p className="text-lg font-medium text-muted-foreground animate-pulse mt-4">
         Loading PIISS...
       </p>
